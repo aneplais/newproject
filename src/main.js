5bfb7769b5d9f7b77c175b544615e5d83657148e
Annette Plaisimont <plaisimont320@hotmail.com>
2020-10-24 19:47:24 +0200

    rajout counter app dans main.js

--- src/main.js
+++ src/main.js
@@ -3,3 +3,15 @@ import App from './App.vue'
 import './index.css'
 
 createApp(App).mount('#app')
+const CounterApp = {
+    data() {
+      return {
+        counter: 0
+      }
+    }
+    mounted() {
+      setInterval(() => {
+        this.counter++
+      }, 1000)
+    }
+  }

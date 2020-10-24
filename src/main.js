b0d06fc875f70be5d6f85e93d017db8d7cec1665
Annette Plaisimont <plaisimont320@hotmail.com>
2020-10-24 19:50:23 +0200

    rajout q dans html

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
+
 import App from './App.vue'
 import './index.css'
 
 createApp(App).mount('#app')
 const CounterApp = {
     data() {
+
       return {
         counter: 0
       }
